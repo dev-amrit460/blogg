8bc8cc7a4d552d461943a91b0d598f329fcba303
dev-amrit460 <go4amrit460@gmail.com>
2020-06-13 00:01:43 +0530

    BLOG

---
+++ index.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDnlkK3uxVUtKoF9YxklHq1GIHhvCcCBjw",
+    authDomain: "blog-amrit.firebaseapp.com",
+    databaseURL: "https://blog-amrit.firebaseio.com",
+    projectId: "blog-amrit",
+    storageBucket: "blog-amrit.appspot.com",
+    messagingSenderId: "689505946104",
+    appId: "1:689505946104:web:4ce295bd7b4ac938746e0d",
+    measurementId: "G-T19G5TKM1R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
